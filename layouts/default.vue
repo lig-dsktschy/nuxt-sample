965cc8ef4b2b4feabe2ed9ab88febeccbbce8aff
tsuchiya_daisuke <tsuchiya_daisuke@liginc.co.jp>
2018-05-14 21:46:07 +0900

    Create common parts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+    <app-header/>
     <nuxt/>
+    <app-footer/>
   </div>
 </template>
+
+<script>
+import AppHeader from '~/components/app-header'
+import AppFooter from '~/components/app-footer'
+
+export default {
+  components: {
+    AppHeader,
+    AppFooter
+  }
+}
+</script>
+
